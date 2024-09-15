1b6f3fb633b10a0b6fd8c2b3e4c5284e596c33b4
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-09-15 21:53:00 +0500

    first commit

---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        String botToken = "7262459338:AAGs8VYvWTWqp947QbqiXama_a6273ZzEHw";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
+            System.out.println("MyAmazingBot successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
