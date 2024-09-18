f7cb74d1ca93a64dc79e4a43a6f659beb5dfb99c
AnyaVoynova <anya.voynova.2005@mail.ru>
2024-09-18 13:55:02 +0500

    feat: amazing bot string changed to povaryoshka bot (#1)
    * feat: amazing bot string changed to povaryoshka bot
    
    * refactor: add was after POVARYOSHKA BOT in Main.java

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String botToken = "7262459338:AAGs8VYvWTWqp947QbqiXama_a6273ZzEHw";
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
-            System.out.println("MyAmazingBot successfully started!");
+            System.out.println("POVARYOSHKA BOT was successfully started!");
             Thread.currentThread().join();
         } catch (Exception e) {
             e.printStackTrace();
