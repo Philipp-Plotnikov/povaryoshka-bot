83cb8d8407b6f8cba0f58fd7a7aab183649235c1
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-09-28 18:54:32 +0500

    refactor: delete not needing line

--- app/src/main/java/core/Main.java
+++ app/src/main/java/core/Main.java
@@ -2,19 +2,16 @@ package core;
 
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
-import models.EnvVars;
 import telegram.bot.PovaryoshkaBot;
 
 public class Main {
 
     public static void main(String[] args) {
         String botToken = System.getenv("TELEGRAM_BOT_API_TOKEN");
-        var a = EnvVars.valueOf("Test");
         try (
             TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()
         ) {
             botsApplication.registerBot(botToken, new PovaryoshkaBot(botToken));
-            System.out.println("PovaryoshkaBot successfully started!");
             Thread.currentThread().join();
         } catch (Exception e) {
             e.printStackTrace();
