3c5d0d9bb875bc015ed0b6a9be49e0f25a02d0d3
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-09-30 16:19:43 +0500

    refactor: delete logger

--- app/src/main/java/core/Main.java
+++ app/src/main/java/core/Main.java
@@ -1,7 +1,5 @@
 package core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 import static models.EnvVars.TELEGRAM_BOT_API_TOKEN;
@@ -9,12 +7,10 @@ import telegram.bot.PovaryoshkaBot;
 
 public class Main {
     public static void main(String[] args) {
-        final Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("Povaryoshka bot started to work");
-        String botToken = System.getenv(TELEGRAM_BOT_API_TOKEN);
         try (
             TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()
         ) {
+            String botToken = System.getenv(TELEGRAM_BOT_API_TOKEN);
             botsApplication.registerBot(botToken, new PovaryoshkaBot(botToken));
             Thread.currentThread().join();
         } catch (Exception e) {
