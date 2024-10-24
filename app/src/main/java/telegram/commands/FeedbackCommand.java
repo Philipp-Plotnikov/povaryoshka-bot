03eb014b4fcdea668e5135fcb553bd081efb6004
AnyaVoynova <anya.voynova.2005@mail.ru>
2024-10-25 00:55:05 +0500

    [NOT READY] feat: create command feedback

--- app/src/main/java/telegram/commands/FeedbackCommand.java
+++ app/src/main/java/telegram/commands/FeedbackCommand.java
@@ -1,12 +1,26 @@
 package telegram.commands;
 
+import java.sql.SQLException;
+import java.util.function.Predicate;
+
+import models.commands.CommandStates;
+import models.db.sqlops.feedback.FeedbackInsertOptions;
+
+import models.db.sqlops.usercontext.UserContextSelectOptions;
+import models.dtos.UserContextDTO;
 import org.telegram.telegrambots.abilitybots.api.objects.Ability;
+
+import static models.commands.MultiStateCommandTypes.FEEDBACK;
 import static org.telegram.telegrambots.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.telegrambots.abilitybots.api.objects.Privacy.PUBLIC;
 import org.telegram.telegrambots.abilitybots.api.util.AbilityExtension;
+import models.db.sqlops.usercontext.UserContextInsertOptions;
 
 import static models.commands.CommandConfig.FEEDBACK_COMMAND_SETTINGS;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
 import telegram.bot.PovaryoshkaBot;
+
 
 public class FeedbackCommand implements AbilityExtension {
     private final PovaryoshkaBot povaryoshkaBot;
@@ -21,9 +35,66 @@ public class FeedbackCommand implements AbilityExtension {
             .info(FEEDBACK_COMMAND_SETTINGS.commandDescription())
             .privacy(PUBLIC)
             .locality(ALL) // ?
-            .action(ctx -> {
-                povaryoshkaBot.getSilent().send("feedback action", ctx.chatId());
-            })
-            .build();
+                .action(ctx -> {
+
+                    try {
+                        povaryoshkaBot.getDbDriver().insertUserContext(
+                                new UserContextInsertOptions(
+                                        ctx.user().getId(),
+                                        FEEDBACK,
+                                        CommandStates.FEEDBACK
+                                )
+                        );
+                        povaryoshkaBot.getSilent().send("В следующем сообщении напишите ваш отзыв о нашем сервисе, и мы его сохраним.", ctx.chatId());
+
+                    } catch(SQLException e) {
+                        System.out.println(e);
+                    }
+                })
+                .reply((action, update) -> {
+                    try {
+                        final UserContextDTO userContextDTO = povaryoshkaBot.getDbDriver().selectUserContext(
+                                new UserContextSelectOptions(
+                                        update.getMessage().getFrom().getId()
+                                )
+                        );
+                        if (userContextDTO.getMultiStateCommandTypes().equals(FEEDBACK)) {
+                            String feedbackText = update.getMessage().getText();
+                            povaryoshkaBot.getDbDriver().insertFeedback(
+                                    new FeedbackInsertOptions(
+                                            update.getMessage().getFrom().getId(),
+                                            feedbackText
+                                    )
+                            );
+                        }
+                    } catch(Exception e) {
+                        System.out.println(e);
+                    }
+
+                },
+                        isFeedbackContext()
+                        )
+                .build();
+
+    }
+
+    private Predicate<Update> isFeedbackContext(){
+        return update -> {
+            boolean isFeedbackContext = false;
+            try {
+                final UserContextDTO userContextDTO = povaryoshkaBot.getDbDriver().selectUserContext(
+                        new UserContextSelectOptions(
+                                update.getMessage().getFrom().getId()
+                        )
+                );
+                if (userContextDTO.getMultiStateCommandTypes() == FEEDBACK) {
+                    isFeedbackContext = true;
+                }
+            } catch(SQLException e) {
+                System.out.println(e);
+            }
+            return isFeedbackContext;
+
+        };
     }
 }
